9edd11aa0ec23b735f9bf185b19dcea1ed4eaf0b
AsianUsianBOlt <asianusainbolt2@gmail.com>
2023-04-03 17:18:12 +1000

    Fixed link to map page

--- demo/src/main/java/example/application/controllers/MainViewController.java
+++ demo/src/main/java/example/application/controllers/MainViewController.java
@@ -43,4 +43,8 @@ public class MainViewController {
         return "redirect:/login";
     }
 
+    @PostMapping("/viewmap")
+    public String viewmap() {
+        return "redirect:/viewmap";
+    }
 }
