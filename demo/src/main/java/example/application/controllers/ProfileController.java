1ef1a23708667c7bd876165a4bed35726b91ec8c
Jaydos421 <48908376+Jaydos421@users.noreply.github.com>
2023-04-17 17:59:59 +1000

    Added profile logic class and related tests.

--- demo/src/main/java/example/application/controllers/ProfileController.java
+++ demo/src/main/java/example/application/controllers/ProfileController.java
@@ -1,6 +1,6 @@
 package example.application.controllers;
 
-
+import example.data.ProfileLogic;
 import example.data.StaticUserDAO;
 import example.data.User;
 import example.data.UserDAO;
@@ -19,6 +19,12 @@ public class ProfileController {
      * database of users.
      */
     private final StaticUserDAO userDAO = new StaticUserDAO();
+
+    /**
+     * The singleton instance of the profile logic class. This is used to access the
+     * logic of the profile screen.
+     */
+    private final ProfileLogic logic = new ProfileLogic();
     private boolean firstTime = true;
 
     private User logged;
@@ -53,21 +59,16 @@ public class ProfileController {
         return "profile-page";
     }
 
+    /**
+     * Saves the user's edited preferences
+     *
+     * @param model The model to add attributes to.
+     * @return The name of the view to display.
+     */
     @RequestMapping(value = "/profile", method = RequestMethod.POST, params = "save")
     public String saveSubmit(@ModelAttribute User user, Model model) {
 
-        User temp = userDAO.getUser(logged.getEmail());
-
-        if (user.getFirstName() != null && !user.getFirstName().isEmpty()){
-            temp.setFirstName(user.getFirstName());
-        }
-
-        if (user.getLastName() != null && !user.getLastName().isEmpty()){
-            temp.setLastName(user.getLastName());
-        }
-
-        userDAO.updateUser(temp);
-
+        logic.processPersonalDetails(logged, user.getFirstName(), user.getLastName(), null);
 
         model.addAttribute("logged", userDAO.getUser(logged.getEmail()));
         model.addAttribute("user", userDAO.getUser(logged.getEmail()));
---
+++ demo/src/test/java/example/application/ProfileTests.java
@@ -0,0 +1,70 @@
+package example.application;
+
+
+import example.data.StaticUserDAO;
+import example.data.User;
+import example.data.ProfileLogic;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileTests {
+
+    private User loggedIn = new User("Jayden Hobbs", "Pass1", "jayden@qut", new double[]{});
+    private ProfileLogic logic = new ProfileLogic();
+    private final StaticUserDAO userDAO = new StaticUserDAO();
+
+
+
+    @BeforeEach @Test
+    public void prelimInit(){
+        loggedIn.setFirstName("Jayden");
+        loggedIn.setLastName("Hobbs");
+        assertEquals(loggedIn.getFirstName(), "Jayden");
+        assertEquals(loggedIn.getEmail(), "jayden@qut");
+    }
+
+    @Test
+    public void testProcessPersonalDetails(){
+        loggedIn.setFirstName("Jayden");
+
+        logic.processPersonalDetails(loggedIn, null, null, "Pass2");
+
+        assertEquals(userDAO.getUser(loggedIn.getEmail()).getFirstName(), "Jayden");
+        assertEquals(userDAO.getUser(loggedIn.getEmail()).getPassword(), "Pass2");
+
+    }
+
+    @Test
+    public void testProcessPreferences(){
+        logic.processPreferences(loggedIn, true, 4, false);
+
+        assertEquals(userDAO.getUser(loggedIn.getEmail()).getPreferences().isDonationNotifications(), true);
+        assertEquals(userDAO.getUser(loggedIn.getEmail()).getPreferences().getDonationRadius(), 4);
+        assertEquals(userDAO.getUser(loggedIn.getEmail()).getPreferences().isExpiryNotifications(), false);
+
+
+    }
+
+    @Test
+    public void testDeleteAccount(){
+        logic.deleteUserAccount(loggedIn);
+
+        assertEquals(userDAO.getUser(loggedIn.getEmail()), null);
+    }
+
+    @Test
+    public void testResetPreferences(){
+        loggedIn.setFirstName("Jayden");
+
+        logic.resetPreferences(loggedIn);
+
+        assertEquals(userDAO.getUser(loggedIn.getEmail()).getPreferences().isDonationNotifications(), true);
+        assertEquals(userDAO.getUser(loggedIn.getEmail()).getPreferences().getDonationRadius(), 5);
+        assertEquals(userDAO.getUser(loggedIn.getEmail()).getFirstName(), "Jayden");
+
+    }
+
+}
