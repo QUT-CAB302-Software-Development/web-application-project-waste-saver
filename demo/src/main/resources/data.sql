ce8bcc10b24f5b0d8567b4057a8a360ed49afd92
Jaydos421 <48908376+Jaydos421@users.noreply.github.com>
2023-05-15 16:22:05 +1000

    Implemented more leaderboard functionality

--- demo/src/main/resources/data.sql
+++ demo/src/main/resources/data.sql
@@ -4,3 +4,4 @@ VALUES
   	('John', 'Some', '123', 'Jsom213@gmail.com', -27.4785, 153.0284),
   	('Will', 'Do', '123', 'DoW1000@gmail.com', -27.4783, 153.0290),
   	('Will', 'Howard','333', 'will222@gmail.com', -27.4788, 153.0279);
+
